594f9e392c189c451c8da38df9ef7ebd6c1dab49
devcen13 <devcen13@gmail.com>
2018-09-28 21:54:34 +0300

    Refactor functional (make everything an iterator)

--- functional.lua
+++ functional.lua
@@ -1,28 +1,58 @@
 local utils = require 'lulz.utils'
 local class = require 'lulz.class'
 local op = require 'lulz.operators'
+local generator = require 'lulz.generator'
+
+local sign = require('lulz.math').sign
 
 local not_implemented = utils.not_implemented
+
 
 local function _id(x)
   return x
 end
 
-local function _map(func, params)
-  local result = {}
-  for k,v in pairs(params) do
-    result[k] = func(v)
+local _range = generator {
+  gen = function(self, start, stop, step)
+    if stop == nil then
+      stop = start
+      start = 1
+    end
+    if step == nil then step = sign(stop - start) end
+
+    local i = start
+    local done = function()
+      if step < 0 then return i < stop end
+      return i > stop
+    end
+    while not done() do
+      self:yield(i)
+      i = i + step
+    end
   end
-  return result
+}
+
+local function _compose(f, g)
+  return function(...) return f(g(...)) end
 end
 
-local function _filter(func, params)
-  local result = {}
-  for _,v in ipairs(params) do
-    if func(v) then table.insert(result, v) end
+local _map = generator {
+  gen = function(func, params)
+    for a,b,c,d,e,f in params do
+      yield(func(a,b,c,d,e,f))
+    end
   end
-  return result
-end
+}
+
+local _filter = generator {
+  gen = function(self, func, params)
+    for a,b,c,d,e,f in params do
+      if func(a,b,c,d,e,f) then
+        self:yield(a,b,c,d,e,f)
+      end
+    end
+  end
+}
 
 local function _foldl(func, list, accum)
   if type(func) == 'string' then func = op[func] end
@@ -55,13 +85,69 @@ function binder:_build_params(...)
 end
 
 
+local _xrepeat = generator {
+  gen = function(self, value)
+    while true do
+      self:yield(value)
+    end
+  end
+}
+
+local _take = generator {
+  gen = function(self, count, iter)
+    local i = 0
+    for a,b,c,d,e,f in iter do
+      if i >= count then return end
+      self:yield(a,b,c,d,e,f)
+      i = i + 1
+    end
+  end
+}
+
+local _skip = generator {
+  gen = function(self, count, iter)
+    local i = 0
+    for a,b,c,d,e,f in iter do
+      if i >= count then
+        self:yield(a,b,c,d,e,f)
+      end
+      i = i + 1
+    end
+  end
+}
+
+local _zip = generator {
+  gen = function(self, iter1, iter2)
+    local item1, item2 = 1
+    while item1 or item2 do
+      item1 = iter1()
+      item2 = iter2()
+      self:yield(item1, item2)
+    end
+  end
+}
+
+
+local _bind = function(func, ...) return binder:new(func, ...) end
 local functional = {
-  id = _id,
-  map = _map,
+  id      = _id,
+  bind    = _bind,
+  compose = _compose,
+
+  zip    = _zip,
+  map    = _map,
   filter = _filter,
-  foldl = _foldl,
-  foldr = not_implemented,
-  bind = function(func, ...) return binder:new(func, ...) end,
+  foldl  = _foldl,
+  foldr  = not_implemented,
+
+  range   = _range,
+  xrange  = _range,
+  xrepeat = _xrepeat,
+  take    = _take,
+  skip    = _skip,
+
+  zeroes = _bind(_xrepeat, 0),
+  ones   = _bind(_xrepeat, 1),
 }
 
 return functional
--- tests/test_functional.lua
+++ tests/test_functional.lua
@@ -1,16 +1,176 @@
-local functional = require 'lulz.functional'
-local TestCase = require 'lulz.tests.testcase'
+local generator = require 'lulz.generator'
+local fn = require 'lulz.functional'
 
-local id = functional.id
-local bind = functional.bind
+local TestCase = require 'lulz.tests.testcase'
 
 
 local TestBind = TestCase:inherit 'Functional Bind'
 
 function TestBind:test_id_bind()
-  self:assert_equal(bind(id)(5), 5)
+  self:assert_equal(fn.bind(fn.id)(5), 5)
 end
 
 function TestBind:test_single_argument_bind()
-  self:assert_equal(bind(id, 5)(), 5)
+  self:assert_equal(fn.bind(fn.id, 5)(), 5)
+end
+
+
+local TestUtils = TestCase:inherit 'Functional Utils'
+
+function TestUtils:test_single_argument_range()
+  local counter = 0
+  local last
+  for i in fn.range(5) do
+    last = i
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 5)
+  self:assert_equal(last, 5)
+end
+
+function TestUtils:test_start_stop_arguments_range()
+  local counter = 0
+  local last
+  for i in fn.range(2, 5) do
+    last = i
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 4)
+  self:assert_equal(last, 5)
+end
+
+function TestUtils:test_start_stop_arguments_negative_range()
+  local counter = 0
+  local last
+  for i in fn.range(2, 0) do
+    last = i
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 3)
+  self:assert_equal(last, 0)
+end
+
+function TestUtils:test_start_stop_step_arguments_range()
+  local counter = 0
+  local last
+  for i in fn.range(2, 5, 0.5) do
+    last = i
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 7)
+  self:assert_equal(last, 5)
+end
+
+function TestUtils:test_start_stop_step_arguments_negative_range()
+  local counter = 0
+  local last
+  for i in fn.range(2, 0, -2) do
+    last = i
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 2)
+  self:assert_equal(last, 0)
+end
+
+function TestUtils:test_xrepeat()
+  local iters = 10000
+  for i in fn.xrepeat(42) do
+    self:assert_equal(i, 42)
+    if iters < 0 then return end
+    iters = iters - 1
+  end
+end
+
+function TestUtils:test_zeroes()
+  local iters = 10000
+  for i in fn.zeroes() do
+    self:assert_equal(i, 0)
+    if iters < 0 then return end
+    iters = iters - 1
+  end
+end
+
+function TestUtils:test_ones()
+  local iters = 10000
+  for i in fn.ones() do
+    self:assert_equal(i, 1)
+    if iters < 0 then return end
+    iters = iters - 1
+  end
+end
+
+function TestUtils:test_take_xrepeat()
+  local iters = 0
+  for i in fn.take(100, fn.xrepeat(42)) do
+    self:assert_equal(i, 42)
+    iters = iters + 1
+  end
+  self:assert_equal(iters, 100)
+end
+
+function TestUtils:test_skip_range()
+  local counter = 0
+  local last
+  for i in fn.skip(45, fn.range(50)) do
+    last = i
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 5)
+  self:assert_equal(last, 50)
+end
+
+function TestUtils:test_zip_equal()
+  local counter = 0
+  for a,b in fn.zip(fn.range(5), fn.range(5)) do
+    self:assert_equal(a, b)
+    counter = counter + 1
+  end
+  self:assert_equal(counter, 5)
+end
+
+
+local TestFilter = TestCase:inherit 'Functional Filter'
+
+function TestFilter:test_filter_true()
+  local iters = 0
+  for _ in fn.filter(function(_) return true end, fn.range(100)) do
+    iters = iters + 1
+  end
+  self:assert_equal(iters, 100)
+end
+
+function TestFilter:test_filter_false()
+  local iters = 0
+  for _ in fn.filter(function(_) return false end, fn.range(100)) do
+    iters = iters + 1
+  end
+  self:assert_equal(iters, 0)
+end
+
+function TestFilter:test_filter_even()
+  local iters = 0
+  for i in fn.filter(function(i) return i % 2 == 0 end, fn.range(100)) do
+    self:assert(i % 2 == 0)
+    iters = iters + 1
+  end
+  self:assert_equal(iters, 50)
+end
+
+function TestFilter:test_filter_ipairs()
+  local iters = 0
+  local iprs = generator {
+    gen = function(self)
+      self:yield(1, 3)
+      self:yield(2, 3)
+      self:yield(3, 3)
+      self:yield(1, 3)
+      self:yield(5, 5)
+      self:yield(8, 8)
+    end
+  }
+  for i,v in fn.filter(function(i, v) return i == v end, iprs()) do
+    self:assert(i == v)
+    iters = iters + 1
+  end
+  self:assert_equal(iters, 3)
 end
