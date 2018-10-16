f68b99d53751cc38267b59c1eb86d2ee63215034
devcen13 <devcen13@gmail.com>
2018-10-16 20:13:12 +0300

    Add initial str.split.

--- str.lua
+++ str.lua
@@ -1,10 +1,35 @@
+local utils = require 'lulz.private.utils'
 local fn = require 'lulz.functional'
+local generator = require 'lulz.generator'
 
 
-local str = {}
+-- str module can be used instead of standard string library or replace it
+local str = utils.clone(string)
+
+str.split = generator {
+  gen = function(self, text, pattern, settings)
+    settings = utils.override({ use_regex = false, skip_empty = false }, settings)
+    pattern = settings.use_regex and pattern or '[^' .. pattern .. ']'
+
+    for match in text:gmatch(pattern) do
+      if not settings.skip_empty or match:len() > 0 then
+        self:yield(match)
+      end
+    end
+  end
+}
 
 function str.join(sep, iterable)
   return fn.foldl(function(a, b) return tostring(a) .. sep .. tostring(b) end, iterable)
 end
+
+function str.words(text)
+  return str.split(text, '%S+', { use_regex = true, skip_empty = true })
+end
+
+function str.lines(text, skip_empty)
+  return str.split(text, '[^(\r?\n)]*', { use_regex = true, skip_empty = skip_empty })
+end
+
 
 return str
--- tests/test_str.lua
+++ tests/test_str.lua
@@ -1,5 +1,7 @@
 local str = require 'lulz.str'
 local fn = require 'lulz.functional'
+local list = require 'lulz.list'
+local utils = require 'lulz.private.utils'
 
 local TestCase = require 'lulz.testcase'
 
@@ -25,3 +27,64 @@ end
 function TestStrJoin:test_iterator_join()
   self:assert_equal(str.join(' ', fn.range(3)), '1 2 3')
 end
+
+
+local TestStrSplit = TestCase:inherit 'Str Split'
+
+TestStrSplit.test_words = TestCase.args_test {
+  call = function(self, text, words)
+    self:assert_equal(list:new(str.words(text)), list(words))
+  end,
+  argsset = {
+    { 'aa, bb,dd  i', { 'aa,', 'bb,dd', 'i' } },
+    {
+[[
+  aaa b  c    d
+  aaa bbbb
+]],
+      {'aaa', 'b', 'c', 'd', 'aaa', 'bbbb'}
+    }
+  }
+}
+
+TestStrSplit.test_lines = TestCase.args_test {
+  call = function(self, text, lines)
+    self:assert_equal(list:new(str.lines(text)), list(lines))
+  end,
+  argsset = {
+    { 'aa, bb,dd  i', { 'aa, bb,dd  i' } },
+    {
+[[aaa b  c    d
+aaa bbbb]],
+      {'aaa b  c    d', 'aaa bbbb' }
+    },
+    {
+[[
+  aaa b  c    d
+
+  aaa bbbb
+]],
+      {'  aaa b  c    d', '', '  aaa bbbb', '' }
+    }
+  }
+}
+
+
+local TestStrReplaceString = TestCase:inherit 'Test replace string with str'
+
+function TestStrReplaceString:setup()
+  self._string = utils.clone(string)
+  utils.extend(string, str)
+end
+
+function TestStrReplaceString:teardown()
+  utils.override(string, self._string)
+  for k,_ in pairs(string) do
+    if self._string[k] == nil then string[k] = nil end
+  end
+end
+
+function TestStrReplaceString:test_self_join()
+  self:assert_equal((' '):join({'a', 'b', 'c'}), 'a b c')
+end
+
