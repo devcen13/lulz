0735d1c4ca99f4377a46e5804163b4d1baa67a31
devcen13 <devcen13@gmail.com>
2019-08-05 11:07:42 +0300

    Add math.inf constant

--- math/utils.lua
+++ math/utils.lua
@@ -4,6 +4,8 @@ local utils = require 'lulz.private.utils'
 -- lulz.math can be used as standard math or replace it
 local math = utils.clone(math)
 
+math.inf = tonumber('inf')
+
 function math.sign(x)
   assert(type(x) == 'number')
   if x < 0 then return -1 end
